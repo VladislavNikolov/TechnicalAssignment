f29dd6c8f9123c893be9267d842f15475ebad1c3
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2018-08-03 11:20:35 +0300

    Implemented CustomersController

---
+++ TA.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿namespace TA.WebApi.Controllers
+{
+    using System.Web.Http;
+    using BLL.Interfaces;
+    using BLL.Services;
+
+    [RoutePrefix("api/customers")]
+    public class CustomersController : ApiController
+    {
+        private readonly ICustomerService customerService;
+
+        public CustomersController()
+        {
+            this.customerService = new CustomerService();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            var customers = this.customerService.GetAll();
+
+            return Ok(customers);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IHttpActionResult GetById(string id)
+        {
+            var customer = this.customerService.GetById(id);
+
+            return Ok(customer);
+        }
+
+        [HttpGet]
+        [Route("{customerId}/orders")]
+        public IHttpActionResult GetOrdersByCustomerId(string customerId)
+        {
+            var orders = this.customerService.GetOrdersByCustomerId(customerId);
+
+            return Ok(orders);
+        }
+    }
+}
