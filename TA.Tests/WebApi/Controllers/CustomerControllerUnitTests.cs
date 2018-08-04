478dfdcb42b7820f424faa7746d3fc2e15d423f9
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2018-08-04 16:40:46 +0300

    Created Tests project

---
+++ TA.Tests/WebApi/Controllers/CustomerControllerUnitTests.cs
@@ -0,0 +1,26 @@
+﻿namespace TA.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using WebApi.Controllers;
+    using Moq;
+    using BLL.Interfaces;
+    using BLL.DTOs;
+    using System.Collections.Generic;
+    using System.Web.Http.Results;
+
+    [TestClass]
+    public class CustomerControllerUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mockedCustomerService = new Mock<ICustomerService>();
+            mockedCustomerService.Setup(x => x.GetAll()).Returns(new List<CustomerDTO>());
+
+            var controller = new CustomersController(mockedCustomerService.Object);
+
+            var response = controller.GetAll() as OkNegotiatedContentResult<IEnumerable<CustomerDTO>>;
+            Assert.IsNotNull(response);
+        }
+    }
+}
