9054b465ef48a24ac810b18762fbaa0a6103db34
VladislavNikolov <vladislav.v.nikolov@gmail.com>
2018-08-05 11:09:38 +0300

    Added some more Unit Tests

--- TA.Tests/MVC/Controllers/CustomerControllerUnitTests.cs
+++ TA.Tests/MVC/Controllers/CustomerControllerUnitTests.cs
@@ -20,7 +20,7 @@
         }
 
         [TestMethod]
-        public void GetAll_WithCustomersReturned_CorrectViewIsReturned()
+        public void Index_WithCustomersReturned_CorrectViewIsReturned()
         {
             mockedCustomerService.Setup(x => x.GetAll(It.IsAny<string>())).Returns(new List<Customer>());
             var controller = new CustomersController(mockedCustomerService.Object);
@@ -31,12 +31,35 @@
         }
 
         [TestMethod]
-        public void GetAll_WithNullReturned_ShouldRedirectToCorrectAction()
+        public void Index_WithNullReturned_ShouldRedirectToCorrectAction()
         {
             mockedCustomerService.Setup(x => x.GetAll(It.IsAny<string>())).Returns((List<Customer>)null);
             var controller = new CustomersController(mockedCustomerService.Object);
 
             var result = controller.Index(null) as RedirectToRouteResult;
+
+            Assert.AreEqual("DisplayError", result.RouteValues["action"]);
+        }
+
+        [TestMethod]
+        public void CustomerDetails_WithOrdersReturned_CorrectViewIsReturned()
+        {
+            mockedCustomerService.Setup(x => x.GetById(It.IsAny<string>())).Returns(new Customer());
+            mockedCustomerService.Setup(x => x.GetOrdersByCustomerId(It.IsAny<string>())).Returns(new List<Order>());
+            var controller = new CustomersController(mockedCustomerService.Object);
+
+            var result = controller.CustomerDetails(string.Empty) as ViewResult;
+
+            Assert.AreEqual("CustomerDetails", result.ViewName);
+        }
+
+        [TestMethod]
+        public void CustomerDetails_WithInvalidCustomerId_ShouldRedirectToCorrectAction()
+        {
+            mockedCustomerService.Setup(x => x.GetById(It.IsAny<string>())).Returns((Customer)null);
+            var controller = new CustomersController(mockedCustomerService.Object);
+
+            var result = controller.CustomerDetails(string.Empty) as RedirectToRouteResult;
 
             Assert.AreEqual("DisplayError", result.RouteValues["action"]);
         }
